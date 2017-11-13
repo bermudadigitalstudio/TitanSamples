71a0db7209b1ec8049fe66ac641c3e20e63770c2
Thomas Catterall <me@swizzlr.co>
2017-11-13 15:08:31 -0500

    Compile against Swift 4.

--- kitura-threading/Package.swift
+++ kitura-threading/Package.swift
@@ -1,10 +1,21 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-  name: "kitura-threading",
-  dependencies: [
-    .Package(url: "https://github.com/bermudadigitalstudio/TitanKituraAdapter.git", majorVersion: 0),
-    .Package(url: "https://github.com/bermudadigitalstudio/Titan", majorVersion: 0)
-  ]
+    name: "kitura-threading",
+    products: [
+        .executable(name: "kitura-threading", targets: ["kitura-threading"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/bermudadigitalstudio/TitanKituraAdapter.git", from: "0.0.0"),
+        .package(url: "https://github.com/bermudadigitalstudio/Titan", from: "0.0.0")
+    ],
+    targets: [
+        .target(
+            name: "kitura-threading",
+            dependencies: [
+                "TitanKituraAdapter",
+                "Titan"
+            ]),
+    ]
 )
